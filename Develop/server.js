db109a352c464c3a12be6473475e1a808370be37
Amanda Lopez <111783227+scorpio1028@users.noreply.github.com>
2022-11-15 19:37:46 -0800

    updated
    added more code to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -4,3 +4,37 @@ const fs = require("fs");
 const util = require("util");
 
 const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const app = express ();
+const PORT = process.env.PORT || 8000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(express.static("./develop/public"));
+
+
+app.get("/api/notes", function (req,res ){
+    readFileAsync(".develop/db/db.json", "utf8").then(funciton(data) {
+        notes = [].concat(JSON.parse(data))
+        res.json(notes)
+    })
+});
+
+app.post("/apt/nptes", function(req,res){
+    const note = req.body;
+    readFileAsync("./develop/db/db.json", "utf8").then(function(data){
+        const notes = [].concat(JSON.parse(data));
+        notes.push(note);
+        return notes
+    }).then(function(notes){
+        writeFileAsync("./develop/db/db.json", JSON.stringify(notes))
+        res.json(note);
+    })
+})
+
+app.delete("/api/note/:id", function(req, res){
+    const idToDelete = parseInt(req.params.id);
+    readFileAsync(".devel")
+})
